4191ddc9efbfd76c0e83213f26447ac821c59191
sternj <binaryman00010@gmail.com>
2018-11-10 15:25:05 -0500

    Made things kinda work

--- __init__.py
+++ __init__.py
@@ -5,15 +5,19 @@ from acrcloud.recognizer import ACRCloudRecognizer
 if os.path.isfile('environ.py'):
     exec(open('./environ.py').read() )
 
-config = {
-        #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
-        'host': os.environ['ACR_HOST'],
-        'access_key': os.environ['ACR_ACCESS_KEY'], 
-        'access_secret': os.environ['ACR_ACCESS_SECRET'],
-        'timeout':10 # seconds
-    }
+    config = {
+            #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
+            'host': os.environ['ACR_HOST'],
+            'access_key': os.environ['ACR_ACCESS_KEY'], 
+            'access_secret': os.environ['ACR_ACCESS_SECRET'],
+            'timeout':10 # seconds
+        }
 recognizer = ACRCloudRecognizer(config)
+
+# Artist
+# Track
 def getGenresFromFile(filename):
-    print(recognizer.recognize_by_file(filename))
+    returned = recognizer.recognize_by_file(filename, 0)
+    return {'artist': returned}
 
 
