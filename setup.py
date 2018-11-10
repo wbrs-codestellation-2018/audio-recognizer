3dcfe6ba96f2aca9ebdbfd6177b01dbdc2899309
sternj <binaryman00010@gmail.com>
2018-11-10 15:47:02 -0500

    Made into package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='audio_recognizer',
+      version='0.1',
+      description='Gets genre info from files',
+      url='https://github.com/wbrs-codestellation-2018/audio-recognizer',
+      author='Sam Stern',
+      author_email='sternj@brandeis.edu',
+      license='MIT',
+      packages=['audio_recognizer'],
+      zip_safe=False)
